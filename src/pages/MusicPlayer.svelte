90218cf301dc5429f4742178b170a4a1b0e9770e
Dale Bendixen <dalebendixen@gmail.com>
2023-02-11 15:38:56 -0500

    added header and footer component to music player page

--- src/pages/MusicPlayer.svelte
+++ src/pages/MusicPlayer.svelte
@@ -1,3 +1,14 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<div>
+  <Header />
+</div>
 <div>
   <h1>MUSIC PLAYER GOES HERE</h1>
+</div>
+<div>
+  <Footer />
 </div>
