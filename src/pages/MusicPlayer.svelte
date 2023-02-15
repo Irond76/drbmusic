7c8972acd8f250e8deded28724053e38466da432
Dale Bendixen <dalebendixen@gmail.com>
2023-02-15 07:29:05 -0500

    added cd image

--- src/pages/MusicPlayer.svelte
+++ src/pages/MusicPlayer.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Link } from "svelte-navigator";
   import songStore from "../stores/songStore";
+  const cdImage =
+    "https://res.cloudinary.com/rush-media/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1676155932/Dale-Bendixen-Music/sexandcostumes_aroro1.jpg";
   let songs = [];
   songStore.subscribe((data) => {
     songs = data;
@@ -12,6 +14,9 @@
     <div class="music-player">
       <div class="text">
         <h3>{song.title}</h3>
+        <div class="cd-image">
+          <img src={cdImage} width="80em" alt="" />
+        </div>
         <div>
           <audio controls autoplay>
             <source src={song.url} type="audio/mpeg" />
@@ -43,6 +48,14 @@
     border-radius: 2em;
     box-shadow: 0px 0px 25em #a00000;
     margin: auto auto;
+  }
+  .cd-image {
+    width: 5em;
+    background-color: black;
+  }
+  .cd-image img {
+    border-radius: 50%;
+    /* border: 3px solid blue; */
   }
 
   @media screen and (max-width: 680px) {
